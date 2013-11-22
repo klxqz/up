ddfc9640893589a10901520a6ed5a623c79f4eff
klxqz <klxqz@mail.ru>
2013-11-22 14:19:26 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,17 +3,16 @@
 return array(
     'name' => 'Вверх',
     'description' => 'Быстрая прокрутка вверх страницы',
-    'vendor'=>903438,
-    'version'=>'1.0.1',
-    'img'=>'img/up.png',
-    'frontend'    => true,
+    'vendor' => '985310',
+    'version' => '1.0.2',
+    'img' => 'img/up.png',
+    'frontend' => true,
     'shop_settings' => true,
-    'icons'=>array(
-        16=>'img/up.png',
+    'icons' => array(
+        16 => 'img/up.png',
     ),
     'handlers' => array(
         'frontend_footer' => 'frontendFooter',
     ),
-
 );
 //EOF
